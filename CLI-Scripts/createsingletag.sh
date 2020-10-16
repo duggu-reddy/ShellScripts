770332f1f3f5b5c206ef3768fd594444b3ac9ca6
Narasimha Reddy Duggu <44774298+duggu-reddy@users.noreply.github.com>
2020-10-16 17:49:09 +0530

    Update createsingletag.sh

--- CLI-Scripts/createsingletag.sh
+++ CLI-Scripts/createsingletag.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#rname="i-08d066aca894cb9e2,i-0158ed579e0e5df57,i-064a69f82a95174e0,i-09b1cf20273d524a9,i-0a6e38c3ed3234b00,i-0c8dfdc5046ce40f7,i-0edd7623fc9836a55"
+#rname="i-08d066axxcb9e2,i-0158edxx5df57,i-064a6xx74e0,i-09b1cf20xxa9,i-0a6e38cxxb00,i-0c8dfdxx0f7,i-0edd7xxa55"
 
 #export IFS=','
 #       for line in ${rname[@]}
